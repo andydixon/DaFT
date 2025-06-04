25063039fbd9713ca93eff9b4a8695cc21e212ca
Andy Dixon <ajdixon@cisco.com>
2025-06-04 06:47:59 +0100

    Improve error handling

--- public/index.php
+++ public/index.php
@@ -6,6 +6,14 @@ use DaFT\Application;
 use DaFT\ErrorHandler;
 
 // Set up the error handler
+if (isset($_GET['errordebug'])) {
+    // If errordebug is set, catch everything (so that notices & warnings go to your handler)
+    error_reporting(E_ALL);
+} else {
+    // If no errordebug, do NOT invoke handler for notices or warnings
+    //   (so PHP will handle E_NOTICE/E_WARNING normally, or suppress if display_errors=Off)
+    error_reporting(E_ALL & ~(E_WARNING | E_NOTICE));
+}
 set_error_handler([ErrorHandler::class, 'handleError']);
 
 // Instantiate and run the application
